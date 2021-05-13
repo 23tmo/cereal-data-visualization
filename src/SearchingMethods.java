2217e3fb0db6ea27f00d539aa5dbe321f38e6c69
23tmo <56054998+23tmo@users.noreply.github.com>
2021-05-13 07:05:11 -0700

    Initial Processing prototype with dataset, record model, and sort/search scaffolding

---
+++ src/SearchingMethods.java
@@ -0,0 +1,20 @@
+public class SearchingMethods {
+
+    public SearchingMethods(){
+    }
+
+    public static int search(Record[] records, String country){
+        return SearchingMethods.linearSearch(records, country);
+        //return SearchingMethods.binarySearch(records, country);
+    }
+
+    private static int linearSearch(Record[] records, String country){
+        // implement this for your data's search criteria
+        return -1;
+    }
+
+    private static int binarySearch(Record[] records, String country) {
+        // implement this for your data's search criteria
+        return -1;
+    }
+}
---
+++ src/SortingMethods.java
@@ -0,0 +1,23 @@
+public class SortingMethods {
+
+    public SortingMethods() {
+    }
+
+    public static void sort(Record[] records){
+        //SortingMethods.selectionSort(records);
+        //SortingMethods.insertionSort(records);
+        SortingMethods.mergeSort(records);
+    }
+
+    private static void selectionSort(Record[] records) {
+        // implement this for your data's sorting criteria
+    }
+
+    private static void insertionSort(Record[] records) {
+        // implement this for your data's sorting criteria
+    }
+
+    private static void mergeSort(Record[] records) {
+        // implement this for your data's sorting criteria
+    }
+}
